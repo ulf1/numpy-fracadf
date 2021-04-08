bf9ad5cda95804014f520f7689c441c57b6e7442
UH <554c46@gmail.com>
2021-04-08 12:34:06 +0200

    pkg version variable added

--- setup.py
+++ setup.py
@@ -2,8 +2,18 @@ from setuptools import setup
 import pypandoc
 
 
+def get_version(path):
+    with open(path, "r") as fp:
+        lines = fp.read()
+    for line in lines.split("\n"):
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
+
 setup(name='numpy-fracadf',
-      version='0.4.0',
+      version=get_version("numpy_fracadf/__init__.py"),
       description='Determine fractal order by the ADF test',
       long_description=pypandoc.convert('README.md', 'rst'),
       url='http://github.com/ulf1/fracadf',
